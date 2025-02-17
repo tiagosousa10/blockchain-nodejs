33ad181be02448f2a6b724397aa1560790dc1f55
tiagosousa <tiagosousa.tams@hotmail.com>
2025-02-17 21:21:55 +0000

    validate and invalidate a block and chain - test

--- blockchain.js
+++ blockchain.js
@@ -13,21 +13,19 @@ class Blockchain {
   }
 
   isValidChain(chain) {
-    if(JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) {
-      return false;
-  }
-
-  for(let i = 1 ; i < chain.length; i++) {
-    const block = chain[i];
-    const lastBlock = chain[i - 1];
-
-    //verifica se o hash do bloco é valido
-    if(block.lastBlock !== lastBlock.hash || block.hash !== Block.blockHash(block)) { 
-      return false
+    if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) return false;
+    for (let i=1; i<chain.length; i++) {
+      const block = chain[i];
+      const lastBlock = chain[i-1];
+      if (
+        block.lastHash !== lastBlock.hash ||
+        block.hash !== Block.blockHash(block)
+      ) {
+        return false;
+      }
     }
+    return true;
   }
-  return true;
-}
 
 }
 
--- blockchain.test.js
+++ blockchain.test.js
@@ -4,9 +4,11 @@ const Block = require('./block.js');
 
 describe('Blockchain', () => {
   let bc; //blockchain
+  let bc2;
 
   beforeEach(() => {
     bc = new Blockchain;
+    bc2 = new Blockchain;
   })
 
   it('starts with genesis block', () => {
@@ -18,5 +20,25 @@ describe('Blockchain', () => {
     bc.addBlock(data)
 
     expect(bc.chain[bc.chain.length - 1].data).toEqual(data)
+  })
+
+  it('validates a valid chain', () => {
+    bc2.addBlock('foo');
+    expect(bc.isValidChain(bc2.chain)).toBe(true);
+  });
+
+  it('invalidates a chain with a corrupt genesis block', () => {  
+    bc2.chain[0].data = '0$';
+    expect(bc.isValidChain(bc2.chain)).toBe(false);
+  });
+
+
+  it('invalidates a corrupt chain', () => {
+
+    bc2.addBlock('200$');
+    bc2.chain[1].data = '333$'; //altera o bloco 1
+
+    expect(bc.isValidChain(bc2.chain)).toBe(false);
+    
   })
 })
