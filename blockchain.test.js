b42b932a1036e2e6496ccccbbc798bf485e621b8
tiagosousa <tiagosousa.tams@hotmail.com>
2025-02-21 21:01:41 +0000

    testing replace methods in the blockchain

--- blockchain.test.js
+++ blockchain.test.js
@@ -41,4 +41,18 @@ describe('Blockchain', () => {
     expect(bc.isValidChain(bc2.chain)).toBe(false);
     
   })
+
+  it('replaces the chain with a valid chain', () => {
+    bc2.addBlock('600$')
+    bc.replaceChain(bc2.chain)
+
+    expect(bc.chain).toEqual(bc2.chain);
+  })
+
+  it('does not replace the chain with one of less or equal length', () => {
+    bc.addBlock('200$')
+    bc.replaceChain(bc2.chain)
+
+    expect(bc.chain).not.toEqual(bc2.chain);
+  })
 })
