da569f3a86837af331cfe4517d71f6846916af7c
tiagosousa <tiagosousa.tams@hotmail.com>
2025-02-21 21:20:33 +0000

    configure express and create the first route to access chain data

--- app/index.js
+++ app/index.js
@@ -1,7 +1,12 @@
-const sum = (a,b) => {
-  if(a <= 0 || b <= 0) {
-    throw new Error('a and b must be positive')
-  } else {
-    return a + b;
-  }
-}
+const express = require('express')
+const Blockchain = require('../blockchain');
+const HTTP_PORT = process.env.HTTP_PORT || 3001;
+
+const app = express()
+const bc = new Blockchain()
+
+app.get('/blocks', (req,res) => {
+  res.json(bc.chain)
+})
+
+app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`))
