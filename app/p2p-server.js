672d3d75977d9569aedb3a7f08ede796ee197e43
tiagosousa <tiagosousa.tams@hotmail.com>
2025-02-23 22:25:23 +0000

    create p2pserver class and first method listen

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -3,3 +3,14 @@ const Websocket = require('ws');
 const P2P_PORT = process.env.P2P_PORT || 5001;
 const peers = process.env.PEERS ? process.env.PEERS.split(',') : [];
 
+class P2pServer {
+  constructor (blockchain) {
+    this.blockchain = blockchain;
+    this.socket = [];
+  }
+};
+
+listen() {
+  const server = new Websocket.Server({port : P2P_PORT})
+  server.on('connection', (socket) => this.connectSocket(socket)) //cria uma conexão com o socket
+}
