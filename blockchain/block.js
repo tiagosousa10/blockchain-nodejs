cd6f4b5651b10ab4392b0237e0637af5c2634810
tiagosousa <tiagosousa.tams@hotmail.com>
2025-03-05 18:35:41 +0000

    adjust Difficulty

--- blockchain/block.js
+++ blockchain/block.js
@@ -19,43 +19,52 @@ class Block {
             lastHash = ${this.lastHash}
             hash = ${this.hash.substring(0, 10)} 
             Nonce = ${this.nonce}
+            Difficulty = ${this.difficulty}
             data: ${this.data}
             `
   }
 
   // use 'new this' to call the constructor
   static genesis() { 
-    return new this('Genesis time', '-------','JFGWADWADWA32',[],0)
+    return new this('Genesis time', '-------','JFGWADWADWA32',[],0, DIFFICULTY)
   }
 
   static mineBlock(lastBlock, data) {
 
     let hash, timestamp; // variáveis locais
     const lastHash = lastBlock.hash; // hash do bloco anterior
+    let {difficulty} = lastBlock; // destructuring
     let nonce = 0;
 
     do {
       nonce++;
       timestamp = Date.now(); 
-      hash = Block.hash(timestamp, lastHash, data, nonce) //using static hash created
-    } while(hash.substring(0,DIFFICULTY) !== '0'.repeat(DIFFICULTY)) // 0 a 4 posições que se repete 4 vezes
+      difficulty= Block.adjustDifficulty(lastBlock, timestamp); //ajusta a dificuldade
+      hash = Block.hash(timestamp, lastHash, data, nonce, difficulty) //using static hash created
+    } while(hash.substring(0,difficulty) !== '0'.repeat(difficulty)) // 0 a 4 posições que se repete 4 vezes
 
-    return new this(timestamp, lastHash, hash, data, nonce);
+    return new this(timestamp, lastHash, hash, data, nonce, difficulty);
   }
 
 
-//SHA - 256 -> hash do bloco
-static hash(timestamp, lastHash, data, nonce, difficulty) {
-  return SHA256(`${timestamp}${lastHash}${data}${nonce}${difficulty}`).toString();
+  //SHA - 256 -> hash do bloco
+  static hash(timestamp, lastHash, data, nonce, difficulty) {
+    return SHA256(`${timestamp}${lastHash}${data}${nonce}${difficulty}`).toString();
+  }
+
+  static blockHash(block) {
+    const {timestamp, lastHash, data, nonce, difficulty} = block; //destructuring
+
+    return Block.hash(timestamp, lastHash, data, nonce, difficulty)
+  }
+
+  static adjustDifficulty(lastBlock, currentTime) {
+    let {difficulty} = lastBlock;
+    difficulty = lastBlock.timestamp + MINE_RATE > currentTime ? difficulty + 1 : difficulty - 1; //ajustar a dificuldade
+
+    return difficulty
+  }
+
 }
-
-static blockHash(block) {
-  const {timestamp, lastHash, data, nonce, difficulty} = block; //destructuring
-
-  return Block.hash(timestamp, lastHash, data, nonce, difficulty)
-}
-
-}
-
 
 module.exports = Block;
