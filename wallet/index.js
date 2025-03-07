1fb8b5baf360416f0df5c6c6402fcc5544993bbc
tiagosousa <tiagosousa.tams@hotmail.com>
2025-03-07 21:58:42 +0000

    toString function added

--- wallet/index.js
+++ wallet/index.js
@@ -4,7 +4,19 @@ const {INITIAL_BALANCE} = require('../config');
 class Wallet {
   constructor() {
     this.balance = INITIAL_BALANCE;
-    this.keyPar = null;
+    this.keyPar = null; 
     this.publicKey = null;
   }
+
+
+  toString() {
+    return `
+      Wallet -
+        publicKey: ${this.publicKey.toString()}
+        balance: ${this.balance}
+    `
+  }
 }
+
+
+module.exports = Wallet;
