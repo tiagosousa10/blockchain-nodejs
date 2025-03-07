d065fbd7bc55e7797ac333aba34faf5cf099192e
tiagosousa <tiagosousa.tams@hotmail.com>
2025-03-07 22:09:12 +0000

    wallet class updated

--- wallet/index.js
+++ wallet/index.js
@@ -1,11 +1,12 @@
+const ChainUtil = require('../chain-util');
 const {INITIAL_BALANCE} = require('../config');
 
 
 class Wallet {
   constructor() {
     this.balance = INITIAL_BALANCE;
-    this.keyPar = null; 
-    this.publicKey = null;
+    this.keyPar = ChainUtil.genKeyPair(); //gera uma chave publica e privada 
+    this.publicKey = this.keyPar.getPublic().encode('hex'); //pega a chave publica e codifica em hexadecimal
   }
 
 
