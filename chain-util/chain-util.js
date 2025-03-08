1560db77b4cedd460f44316a97d81b1c62ef8c27
tiagosousa <tiagosousa.tams@hotmail.com>
2025-03-08 20:14:51 +0000

    adding the uuid

--- chain-util/chain-util.js
+++ chain-util/chain-util.js
@@ -1,11 +1,15 @@
 const EC = require('elliptic').ec;
+const uuidV1 = require('uuid/v1');
 const ec = new EC('secp256k1'); // bitcoin use secp256k1
 
 class ChainUtil{ //utilidade da cadeia de blocos
 
   static genKeyPair(){
     return ec.genKeyPair() //gera uma chave publica e privada
+  }
 
+  static id() {
+    return uuidV1(); //gera um uuid
   }
 
 }
