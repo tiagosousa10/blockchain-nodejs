af65255be054cdec8cd886c9f231842a5895235c
tiagosousa <tiagosousa.tams@hotmail.com>
2025-03-18 15:50:47 +0000

    add transactions to the pool

---
+++ wallet/transaction-pool.js
@@ -0,0 +1,18 @@
+class TransactionPool {
+  constructor () {
+    this.transactions = [];
+  }
+
+  updateOrAddTransaction(transaction){
+    // se a transação já existir, atualiza ela
+    // se não existir, adiciona a transação
+    let transactionWithId = this.transactions.find(t => t.id === transaction.id)
+
+    if(transactionWithId){
+      this.transactions[this.transactions.indexOf(transactionWithId)] = transaction; // pega o index da transação
+
+    } else {
+      this.transactions.push(transaction) // adiciona a transação
+    }
+  }
+}
