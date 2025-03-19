0d51f8c7b94f1cdd33d3bb076409dbde9d2b5508
tiagosousa <tiagosousa.tams@hotmail.com>
2025-03-19 18:21:50 +0000

    testing with jest the wallet transactions

---
+++ wallet/index.test.js
@@ -0,0 +1,44 @@
+const { beforeEach } = require('node:test');
+const Wallet = require('./index');
+const TransactionPool = require('./transaction-pool')
+
+describe('Wallet', () => {
+  let wallet, tp;
+
+  beforeEach(() => {
+    wallet = new Wallet();
+    tp =  new TransactionPool();
+  })
+
+  describe('createTransaction', () => {
+    let transaction, sendAmount, recipient;
+
+    beforeEach(() => {
+      sendAmount = 50;
+      recipient = 'random-addr3ss';
+      transaction = wallet.createTransaction(recipient, sendAmount, tp)
+    })
+
+    describe('and doing the same transaction',() => {
+      beforeEach(() => {
+        wallet.createTransaction(recipient, sendAmount, tp)
+      })
+
+      it('doubles the `sendAmount`subtracted from the wallet balance', () => {
+        if(transaction) {
+          expect(transaction.outputs.find(output => output.address === wallet.publicKey).amount).toEqual(wallet.balance - sendAmount * 2)
+        } else {
+          expect(transaction).toBeUndefined()
+        }
+      })
+
+      it('clones the `sendAmount` output for the recipient', () => {
+        if(transaction) {
+          expect(transaction.outputs.filter(output => output.address === recipient).map(output => output.amount)).toEqual([sendAmount,sendAmount])
+        }  else {
+          expect(transaction).toBeUndefined()
+        }
+      })
+    })
+  })
+})
